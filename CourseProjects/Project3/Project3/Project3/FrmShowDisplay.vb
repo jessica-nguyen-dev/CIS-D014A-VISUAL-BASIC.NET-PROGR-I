e327c2dfd93a394ada7f5bd973c6e83d1fbe603f
jessica-nguyen-dev <jessica.u.nguyen@sjsu.edu>
2024-09-04 13:16:23 -0700

    Delete CourseProjects/Project3/Project3/Project3 directory

--- CourseProjects/Project3/Project3/Project3/FrmShowDisplay.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class FrmShowDisplay
-  Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
-    'Close History Form
-    Me.Close()
-
-  End Sub
-
-End Class
