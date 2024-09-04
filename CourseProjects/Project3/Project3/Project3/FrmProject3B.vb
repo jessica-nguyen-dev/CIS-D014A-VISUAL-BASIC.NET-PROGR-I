82f345f9c03c5ea2cd76e079f689287376254599
jessica-nguyen-dev <jessica.u.nguyen@sjsu.edu>
2024-09-04 13:17:19 -0700

    Add files via upload

---
+++ CourseProjects/Project3/Project3/Project3/FrmProject3B.vb
@@ -0,0 +1,144 @@
+﻿Public Class FrmProject3B
+
+  '********************************************************************************************************
+  '*** Program: Project3
+  '*** Author : Jessica Nguyen
+  '*** Date   : 11/12/2023
+  '***
+  '*** Purpose: Determine if a word or phrase is a palindrome.
+  '***
+  '*** Prompt: The program requests user to input a word or phrase and then determines if
+  '*** it is a palindrome, disregarding any non alpha-numeric characters!
+  '********************************************************************************************************
+
+  'Class Level Variables and Constants
+  Dim lstHistory As New List(Of String)
+
+  Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+    'Reset Program
+    Call Reset()
+
+  End Sub
+
+  Private Sub FrmProject3B_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    'Reset Program on Form Load
+    Call Reset()
+
+  End Sub
+
+  Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+    'Close Program
+    Me.Close()
+
+  End Sub
+
+  Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+    'Checks if user input is a palindrome, then adds result to lstHistory
+
+    'Declare Variables
+    Dim strIn As String
+    Dim strCheck As String
+
+    'Gather User Input
+    strIn = CStr(txtInput.Text)
+
+    'Verify Textbox is Not Empty
+    If strIn.Length = 0 Then
+      lblInfo.Text = "Must enter a word or phrase"
+      Exit Sub
+    End If
+
+    'Convert strIn to ONLY Alphanumeric Characters 
+    strCheck = ReturnOnlyAlhpa_Numeric(strIn)
+
+    'Check if Input is a Palimdrome
+    If IsPalindrome(strCheck) Then
+      lblInfo.Text = "Is a Palindrome"
+      lstHistory.Add(strIn & " " & lblInfo.Text)
+    Else
+      lblInfo.Text = "Is Not a palindrome"
+      lstHistory.Add(strIn & " " & lblInfo.Text)
+    End If
+
+  End Sub
+
+  Private Sub Reset()
+    'Reset Program
+    lstHistory.Clear()
+    lblInfo.Text = "Nothing to display yet."
+    txtInput.Text = ""
+    txtInput.Focus()
+
+  End Sub
+
+  Private Function IsPalindrome(ByVal strCheck As String) As Boolean
+    'Checks if a word or phrase is a palindrome by seeing if its the same as its reverse
+
+    'Declare Required Variables
+    Dim strReverse As String = ""
+
+    'Build String in Reverse
+    For i As Integer = strCheck.Length - 1 To 0 Step -1
+      strReverse &= strCheck(i)
+    Next
+
+    'Compare strReverse to strCheck
+    If strReverse = strCheck Then
+      Return True
+    Else
+      Return False
+    End If
+
+  End Function
+
+
+  Private Function ReturnOnlyAlhpa_Numeric(ByVal strIn As String) As String
+    'Converts user input to uppercase, and removes non-alphanumeric characters
+
+    'Decleare strOut (Variable to be Returned)
+    Dim strOut As String = ""
+
+    'Convert Original String to Uppercase
+    strIn = strIn.ToUpper
+
+    'Build strOut Using Only Letters and Numbers
+    For i As Integer = 0 To strIn.Length - 1
+
+      If ((strIn(i) >= "A") And (strIn(i) <= "Z")) Or ((strIn(i) >= "0") And (strIn(i) <= "9")) Then
+        strOut &= strIn(i)
+
+      End If
+
+    Next
+
+    'Return Result 
+    Return strOut
+
+  End Function
+
+  Private Sub btnHistory_Click(sender As Object, e As EventArgs) Handles btnHistory.Click
+    'Displays history of all checked words and phrases
+
+    'Create New Form
+    Dim frmDisp As New FrmShowDisplay
+
+    'Exits Sub If List Box is Not Empty
+    If lstHistory.Count = 0 Then
+      lblInfo.Text = "Nothing to display yet."
+      Exit Sub
+    End If
+
+    'Clear List Box From Last Invocation
+    frmDisp.lstHistory.Items.Clear()
+
+    'Copy all items from listbox to frmDisp's listbox
+    For i As Integer = 0 To lstHistory.Count - 1
+      frmDisp.lstHistory.Items.Add((i + 1) & ": " & lstHistory(i))
+    Next
+
+    'Display History
+    frmDisp.ShowDialog()
+
+  End Sub
+
+End Class
---
+++ CourseProjects/Project3/Project3/Project3/FrmShowDisplay.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmShowDisplay
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    lstHistory = New ListBox()
+    btnClose = New Button()
+    SuspendLayout()
+    ' 
+    ' lstHistory
+    ' 
+    lstHistory.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+    lstHistory.FormattingEnabled = True
+    lstHistory.ItemHeight = 32
+    lstHistory.Location = New Point(56, 68)
+    lstHistory.Name = "lstHistory"
+    lstHistory.Size = New Size(939, 452)
+    lstHistory.TabIndex = 0
+    ' 
+    ' btnClose
+    ' 
+    btnClose.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+    btnClose.Location = New Point(418, 555)
+    btnClose.Name = "btnClose"
+    btnClose.Size = New Size(217, 109)
+    btnClose.TabIndex = 1
+    btnClose.Text = "Close"
+    btnClose.UseVisualStyleBackColor = True
+    ' 
+    ' FrmShowDisplay
+    ' 
+    AutoScaleDimensions = New SizeF(10F, 25F)
+    AutoScaleMode = AutoScaleMode.Font
+    ClientSize = New Size(1052, 704)
+    Controls.Add(btnClose)
+    Controls.Add(lstHistory)
+    Name = "FrmShowDisplay"
+    Text = "FrmHistory"
+    ResumeLayout(False)
+  End Sub
+
+  Friend WithEvents lstHistory As ListBox
+  Friend WithEvents btnClose As Button
+End Class
